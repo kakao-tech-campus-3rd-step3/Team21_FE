5f124d742efa458c832d0de35e7592aa140cf38f
jmj <waysoleward01@naver.com>
2025-09-04 19:31:35 +0900

    chore: main.tsx 위치 변경 - 루트 -> app/

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Team21_FE
+
+UniScope FSD 가이드
+
+1. index.ts 에서는 import 만 가져다 쓰기
+2. 디렉토리명은 기본적으로는 utils 이런느낌인데 동사형이면 UnivCompare 이런느낌으로 쓰기
+3.
--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,7 +3,7 @@ import "@/app/styles/global.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./app";
+import App from "@/app";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
