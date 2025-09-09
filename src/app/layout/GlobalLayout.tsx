81c7699ad3c352d01d22ef23750e9ae503850e69
jmj <waysoleward01@naver.com>
2025-09-09 16:21:32 +0900

    feat: 공통 layout 네비바 추가

--- src/app/layout/GlobalLayout.tsx
+++ src/app/layout/GlobalLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 
+import { NavigationBar } from "@/widgets/navigation-bar";
 import { Footer } from "@/widgets/site-footer";
 import { Header } from "@/widgets/site-header";
 
@@ -7,6 +8,7 @@ export function GlobalLayout() {
   return (
     <>
       <Header />
+      <NavigationBar />
       <main>
         <Outlet />
       </main>
