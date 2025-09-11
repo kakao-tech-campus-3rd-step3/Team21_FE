636c7153690276e0b9dcb9fc8527359f708caf10
LeeHeeSung98989898 <goqpqp234@gmail.com>
2025-09-12 02:17:31 +0900

    feat: NavTrail의 Crumbs관련 변경

--- src/features/nav-trail/index.ts
+++ src/features/nav-trail/index.ts
@@ -1,3 +1,3 @@
-export type { Crumb } from "@/features/nav-trail/model";
-export { buildCrumbs } from "@/features/nav-trail/model";
+export { buildCrumbs } from "@/features/nav-trail/lib/buildCrumbs";
+export type { Crumb } from "@/features/nav-trail/model/crumb";
 export { default as NavTrail } from "@/features/nav-trail/ui/NavTrail";
