d9bf777792d29b0caa5e4c994f02804c0a21c0b6
LeeHeeSung98989898 <goqpqp234@gmail.com>
2025-09-11 16:11:40 +0900

    feat: GlobalLayout에 적용

--- src/app/layout/GlobalLayout.tsx
+++ src/app/layout/GlobalLayout.tsx
@@ -1,18 +1,22 @@
 import { Outlet } from "react-router-dom";
 
+import { BackgroundDecor } from "@/shared/ui/BackgroundDecor";
 import { NavigationBar } from "@/widgets/navigation-bar";
 import { Footer } from "@/widgets/site-footer";
 import { Header } from "@/widgets/site-header";
 
 export function GlobalLayout() {
   return (
-    <>
-      <Header />
-      <NavigationBar />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
-    </>
+    <div className="relative min-h-screen text-foreground">
+      <BackgroundDecor />
+      <div className="relative z-10">
+        <Header />
+        <NavigationBar />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
   );
 }
