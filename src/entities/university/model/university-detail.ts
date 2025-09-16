d9c6bf317322e3155bf9798b8a4cc2c2f023a703
jmj <waysoleward01@naver.com>
2025-09-16 16:46:30 +0900

    feat: 주석 추가

--- src/entities/university/model/university-detail.ts
+++ src/entities/university/model/university-detail.ts
@@ -3,6 +3,10 @@
  * 도메인 모델을 정의합니다.
  * api 요청에 대한 응답은 ~~Response 로 따로 정의합니다.
  * Response -> 도메인 모델 -> 뷰 모델
+ *
+ * 서버 스키마를 그대로 받는건 ~~Response
+ * 프론트엔드의 도메인 스키마는 university-detail
+ * 특정 섹션에서만 사용하는 데이터는 뷰 모델
  */
 
 export type UniversityKeyStats = {
