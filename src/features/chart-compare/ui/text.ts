8f231dda81aaac70b64ee928e9d99c092e7119c0
Yebeen <yebeenlee0503@naver.com>
2025-09-18 14:29:55 +0900

    feat : 텍스트 추가

--- src/features/chart-compare/ui/text.ts
+++ src/features/chart-compare/ui/text.ts
@@ -14,3 +14,20 @@ export const COMPARE_LINE_CHART_TEXTS = {
   title: "교수 평점 변화 추이",
   description: "2021 - 2025",
 };
+
+export const COMPARE_UNIV_RADER_CHART_TEXTS = {
+  title: "대학교 세부 항목 비교",
+  description: "어떤 대학교를 선택해야 할까?",
+  categories: {
+    food: "학식",
+    dorm: "기숙사",
+    conv: "편의시설",
+    campus: "캠퍼스 환경",
+    welfare: "학생 복지",
+  },
+};
+
+export const COMPARE_UNIV_LINE_CHART_TEXTS = {
+  title: "대학교 종합 평점 변화 추이",
+  description: "2021 - 2025",
+};
