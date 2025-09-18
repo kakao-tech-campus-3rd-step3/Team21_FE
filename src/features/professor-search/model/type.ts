6b6e3d13d498e2e94889436b62e65878b99b8303
Yebeen <yebeenlee0503@naver.com>
2025-09-18 12:24:15 +0900

    remove : 삭제 파일 반영

--- src/features/professor-search/model/type.ts
+++
@@ -1,7 +0,0 @@
-export type ProfessorSearch = {
-  id: string;
-  name: string;
-  univ: string;
-  dept: string;
-  initials?: string;
-};
--- src/features/professor-search/texts/index.ts
+++
@@ -1,6 +0,0 @@
-export const SEARCH_PROFESSOR_TEXTS = {
-  cardTitle: "비교할 교수 검색",
-  cardDescription: "교수명, 학과명, 대학명으로 검색해보세요.",
-  placeholder: "검색…",
-  searchButton: "검색",
-};
