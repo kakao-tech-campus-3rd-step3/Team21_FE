4a9781b6c7b80289aedc7951c1084efea69b4ff0
Yebeen <yebeenlee0503@naver.com>
2025-09-18 11:51:38 +0900

    remove : 삭제 변경사항

--- src/entities/professor/lib/getAverage.ts
+++
@@ -1,5 +0,0 @@
-export const getAverage = (arr: number[]): number => {
-  if (!arr.length) return 0;
-  const sum = arr.reduce((acc, val) => acc + val, 0);
-  return parseFloat((sum / arr.length).toFixed(1));
-};
