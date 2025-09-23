8cca0a5e3908ff129189dde0c7ce6fc03e655612
LeeHeeSung98989898 <goqpqp234@gmail.com>
2025-09-23 22:27:29 +0900

    feat: 교수 평가 페이지 작성

---
+++ src/pages/professor-eval/text.ts
@@ -0,0 +1,10 @@
+export const PROFESSOR_EVAL_TEXT = {
+  title: "교수 평가",
+  paperEval: "논문 평가",
+  paperComment: "논문에 대한 의견 (선택)",
+  paperPlaceholder: "논문에 대한 생각을 자유롭게 작성해주세요",
+  labEval: "연구실 평가",
+  validate: {
+    requiredStar: "별점을 선택해주세요.",
+  },
+};
