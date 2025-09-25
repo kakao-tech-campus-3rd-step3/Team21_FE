682328a03a1787e08fef783db5814b3e4dcf7d1a
Yebeen <yebeenlee0503@naver.com>
2025-09-26 01:05:57 +0900

    feat : index.ts 추가

--- src/features/search/index.ts
+++ src/features/search/index.ts
@@ -1,2 +1,3 @@
+export { ProfessorResultItem, UniversityResultItem } from "@/features/search/ui/SearchItemList";
 export { SearchResultsList } from "@/features/search/ui/SearchResultList";
 export { SearchUniversity } from "@/features/search/ui/SearchUniversity";
