f6bb5ccaf5b9a603fe738e6ed7bfba038b592110
jmj <waysoleward01@naver.com>
2025-09-30 16:04:05 +0900

    feat: storybook 설정 진행

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from "@eslint/js";
 import prettier from "eslint-config-prettier";
 import reactHooks from "eslint-plugin-react-hooks";
@@ -32,4 +35,4 @@ export default tseslint.config([
       "react-refresh/only-export-components": "off",
     },
   },
-]);
+], storybook.configs["flat/recommended"]);
