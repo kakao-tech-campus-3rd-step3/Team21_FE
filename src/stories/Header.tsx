7d8379b9e0ee517b1b65d9733e79d7354c7438d7
jmj <waysoleward01@naver.com>
2025-09-30 17:09:03 +0900

    feat: fix lint error

--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -1,8 +1,8 @@
-import './header.css';
+import "./header.css";
 
-import React from 'react';
+import React from "react";
 
-import { Button } from './Button';
+import { Button } from "./Button";
 
 type User = {
   name: string;
