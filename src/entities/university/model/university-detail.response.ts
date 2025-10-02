5608bc7ee76296308cbe4ccbeddb6458e080b473
jmj <waysoleward01@naver.com>
2025-10-02 17:04:40 +0900

    feat: 학교 상세 api 객체 정의

---
+++ src/entities/university/model/university-detail.response.ts
@@ -0,0 +1,13 @@
+export type UniversityDetailResponse = {
+  university: {
+    univSeq: number;
+    name: string;
+    address: string;
+    tel: string;
+    homePage: string;
+    image: string;
+    establishedYear: string;
+    totalStudent: number;
+    campusCnt: number;
+  };
+};
