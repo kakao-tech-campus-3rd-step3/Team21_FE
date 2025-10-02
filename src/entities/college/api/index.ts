a9867a3fd058f9a1e6ebc0f44694ae61883a840c
jmj <waysoleward01@naver.com>
2025-10-02 18:21:49 +0900

    feat: 단과대학 상세페이지 api 함수 작성

--- src/entities/college/api/index.ts
+++ src/entities/college/api/index.ts
@@ -1,7 +1,13 @@
+import type { CollegeDetailResponse } from "@/entities/college/model/college-detail.response";
 import { apiClient } from "@/shared/api/apiClient";
 import type { CollegeListResponse } from "../model/college-list.response";
 
 export async function fetchCollegesByUniversity(univSeq: number) {
   const { data } = await apiClient.get<CollegeListResponse>(`/api/univ/${univSeq}/colleges`);
+  return data;
+}
+
+export async function fetchCollegeDetail(collegeSeq: number) {
+  const { data } = await apiClient.get<CollegeDetailResponse>(`/api/college/${collegeSeq}/details`);
   return data;
 }
