2baaf773f83d5aaec53640478181043c447bb4c7
jmj <waysoleward01@naver.com>
2025-10-06 16:31:35 +0900

    feat: 교수리스트 api 모델 작성

---
+++ src/entities/department/model/department-list.response.ts
@@ -0,0 +1,16 @@
+export type DepartmentListResponse = {
+  departments: Array<{
+    deptSeq: number;
+    deptName: string;
+    homePage: string | null;
+    deptAddress: string | null;
+    deptTel: string | null;
+    deptFax: string | null;
+    deptEmail: string | null;
+    deptEstablishedYear: string | null;
+    deptIntro: string | null;
+    deptStudentNum: number | null;
+    professorCount: number | null;
+    employmentRate: number | null;
+  }>;
+};
