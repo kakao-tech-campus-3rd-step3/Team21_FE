730897b9265a08d63df135392fc317026487f4ad
jmj <waysoleward01@naver.com>
2025-10-08 14:34:26 +0900

    feat: fix lint error

--- src/entities/department/hooks/useDepartmentsByCollege.ts
+++ src/entities/department/hooks/useDepartmentsByCollege.ts
@@ -1,7 +1,8 @@
+import { useQuery } from "@tanstack/react-query";
+
 import { fetchDepartmentsByCollege } from "@/entities/department/api";
 import type { Department } from "@/entities/department/model/department-list.domain";
 import { mapDepartmentListResponseToDomain } from "@/entities/department/model/department-list.map";
-import { useQuery } from "@tanstack/react-query";
 
 export function useDepartmentsByCollege(collegeSeq: number) {
   return useQuery<Department[]>({
