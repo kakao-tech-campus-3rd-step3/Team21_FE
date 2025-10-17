9eb5487ebb5d6c99b8333aee9303d4d5c2da24d1
Yebeen <yebeenlee0503@naver.com>
2025-10-17 16:10:19 +0900

    refactor : 엔드포인트 수정

--- src/shared/config/routes.ts
+++ src/shared/config/routes.ts
@@ -7,7 +7,7 @@ export const ROUTES = {
   DEPARTMENT_DETAIL: (id: string | number = ":id") => `/department/${id}`,
   COMPARE_PROFESSOR: (id: string | number = ":id") => `/compare/${id}/professor/`,
   UNIV_SEARCH_RESULT: (id: string | number = ":id") => `/search-result/university/${id}`,
-  COMPARE_UNIVERSITY: (id: string | number = ":id") => `/compare/${id}/university/`,
+  COMPARE_UNIVERSITY: (id: string | number = ":id") => `/compare/university/${id}`,
   PROFESSOR_EVAL: (id: string | number = ":id") => `/professor/${id}/evaluate`,
   UNIVERSITY_EVAL: (id: string | number = ":id") => `/university/${id}/evaluate`,
   COURSE_EVAL: (lecSeq: string | number = ":lecSeq") => `/course/${lecSeq}/evaluate`,
